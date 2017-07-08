dcdbd66507d6119db6d42dbd13562787149c420b
brinuk <brinuk@users.noreply.github.com>
2017-07-08 14:04:57 +0100

    Arduino - Pairs HC05 module with BT device
    Use Arduino to pair a HC05 module with a Bluetooth device. If the BT device is a GPS module that sends NMEA sentences the library will parse GGA and GSA sentences.
    
    Basic Bluetooth sketch connects (at 38400) the HC05 to a BT device
    and communicate using the serial monitor at 9600
    Connect HC05 VCC to Arduino pin 12
    Connect HC05 EN to Arduino pin 11
    Connect the HC05 GND to GND
    Connect the HC-05 TX to Arduino pin 2 RX.
    Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
    
    See example for methods.

---
+++ HC05Bind/HC05Bind.h
@@ -0,0 +1,75 @@
+/*
+  MyBlankLib.h will square any number.
+*/
+
+#ifndef HC05Bind_h
+#define Hc05Bind_h
+
+/*-----( Import needed libraries )-----*/
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include "SoftwareSerial.h"
+
+/*-----( Declare Constants and Pin Numbers )-----*/
+
+#define HC05PowerPin 12 //power to HC05
+#define HC05AtPin 11 //puts HC05 into command mode
+
+class HC05Bind
+{
+/*-----( Declare Variables and functions )-----*/
+  public:
+    HC05Bind();
+	bool search();
+	bool begin(String pw);
+	void comms();
+	void gps();
+	bool available;
+	String NMEAphrase;
+	bool fix = false;
+	int latDeg;
+	float latMin;
+	int longDeg;
+	float longMin;
+	String latSector;
+	String longSector;
+	String UTC;
+	float altitude;
+	int tracked;
+	String PRNs;
+	bool paired = false;
+	
+
+	
+    
+  private:
+	String readHC05();
+	void sendAtCommand(String command);
+	void setHC05ToCommandMode();
+	void writeString(String stringData);
+	bool clearPairings();
+	bool setToMaster();
+	bool setToAnyBT();
+	bool setToEnquiry();
+	void SppProfile();
+	bool setPassword(String pw);
+	bool find();
+	bool locate();
+	String reply;
+	bool started = false;
+	bool ended = true;
+	char c;
+	int countChars = 0;
+	String phrase;
+	String getChecksum(String sentence);
+	bool isValid(String sentence);
+	int findComma(int nthComa);
+	
+};
+
+#endif
+
